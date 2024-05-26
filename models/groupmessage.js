2d166b71e84b0aa952433d4b7174ac7f93d35bb2
Suvarnasrayshi <suvarnasrayshi.sinha.2024@gmail.com>
2024-05-27 00:16:21 +0530

    initial commit

---
+++ models/groupmessage.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class groupmessage extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      groupmessage.belongsTo(models.group, { foreignKey: 'group_id' });
+
+      groupmessage.belongsTo(models.user, { foreignKey: 'sender_id' });
+    }
+  }
+  groupmessage.init({
+    group_id: DataTypes.INTEGER,
+    sender_id: DataTypes.INTEGER,
+    content: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'groupmessage',
+  });
+  return groupmessage;
+};
